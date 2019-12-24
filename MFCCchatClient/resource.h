0fc84a58ce956ccea68f61b6aa40aa5d96a97a8e
yangyang223 <1412799935@qq.com>
2019-12-24 11:57:45 +0800

    shangchaun

---
+++ MFCCchatClient/resource.h
@@ -0,0 +1,32 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 MFCCchatClient.rc 使用
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_MFCCCHATCLIENT_DIALOG       102
+#define IDR_MAINFRAME                   128
+#define IDC_SEND_BTN                    1000
+#define IDC_CONNECT_BTN                 1001
+#define IDC_DISCONNECT_BTN              1002
+#define IDC_CLEARMSG_BTN                1003
+#define IDC_SENDMSG_EDIT                1004
+#define IDC_PORT_EDIT                   1005
+#define IDC_AUTOSENDMSG_EDIT            1006
+#define IDC_COLUR_COMBO1                1007
+#define IDC_LIST1                       1008
+#define IDC_AUTOSEND_RADIO1             1009
+#define IDC_IPADDRESS1                  1010
+#define IDC_CHECK1                      1011
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1013
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
